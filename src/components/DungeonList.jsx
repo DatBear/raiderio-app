3f864863188664b7899fb3b1568062bb769bf51f
Joe Rankin <jrankin312@gmail.com>
2018-05-24 19:03:47 -0500

    Fixed the table to show a column for each character.

--- src/components/DungeonList.jsx
+++ src/components/DungeonList.jsx
@@ -11,13 +11,8 @@ export default class DungeonList extends React.Component {
     }
 
     render(){
-        //ugh.. pretty sure I want to change dungeonOrder to be complex objects with just short_name and name, then use that to loop through and find dungeons for each column from there
-        //gotta go to work tho...
         var dungeonOrder = this.props.dungeonOrder;
         var dungeons = this.props.profiles && this.props.profiles.map(this.props.getDungeons);
-        var dungeonSort = (a, b) => {
-            return dungeonOrder.indexOf(a.short_name) < dungeonOrder.indexOf(b.short_name) ? -1 : 1;
-        };
         var sortedProfiles = this.props.profiles && this.props.profiles.sort((a, b) => {
             return this.characterSortOrder(...a.character) < this.characterSortOrder(...b.character) ? -1 : 1;
         })
@@ -34,9 +29,15 @@ export default class DungeonList extends React.Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {dungeons && dungeons.map((dungeonList, i) => dungeonList.map((dungeon, j) => <DungeonRow key={j} dungeon={dungeon}>
-                            {<DungeonScore dungeon={dungeons[i][j]} />}
-                        </DungeonRow>))}
+                        {dungeonOrder.map((dungeon, dIdx) => (
+                            <DungeonRow key={dIdx} dungeon={dungeon}>
+                                {dungeons.map((dungeonList, dlIdx) => (
+                                    dungeonList.filter(d => d.short_name == dungeon.short_name).map((dungeonScore, dSIdx) => (
+                                        <DungeonScore key={dSIdx} dungeon={dungeonScore}/>
+                                    ))
+                                ))}
+                            </DungeonRow>
+                        ))}
                     </tbody>
                 </table>
             </div>
--- src/reducers/RaiderIoReducer.js
+++ src/reducers/RaiderIoReducer.js
@@ -5,19 +5,19 @@ const defaultState = {
     profiles: [],
     characters: [],
     dungeonOrder: [
-        'ARC',
-        'BRH',
-        'COEN',
-        'COS',
-        'DHT',
-        'EOA',
-        'HOV',
-        'LOWR',
-        'MOS',
-        'NL',
-        'SEAT',
-        'UPPR',
-        'VOTW'
+        { short_name:'ARC',  dungeon: 'The Arcway'},
+        { short_name:'BRH',  dungeon: 'Black Rook Hold'},
+        { short_name:'COEN', dungeon: 'Cathedral of Eternal Night'},
+        { short_name:'COS',  dungeon: 'Court of Stars'},
+        { short_name:'DHT',  dungeon: 'Darkheart Thicket'},
+        { short_name:'EOA',  dungeon: 'Eye of Azshara'},
+        { short_name:'HOV',  dungeon: 'Halls of Valor'},
+        { short_name:'LOWR', dungeon: 'Return to Karazhan: Lower'},
+        { short_name:'MOS',  dungeon: 'Maw of Souls'},
+        { short_name:'NL',   dungeon: 'Neltharion\'s Lair'},
+        { short_name:'SEAT', dungeon: 'Seat of the Triumvirate'},
+        { short_name:'UPPR', dungeon: 'Return to Karazhan: Upper'},
+        { short_name:'VOTW', dungeon: 'Vault of the Wardens'},
     ]
 }
 
